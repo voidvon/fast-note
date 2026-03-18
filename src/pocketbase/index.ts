057633dc25aae304a145e1aabab13e8e3c046bfc
voidvon <x@0122.vip>
2026-03-18 12:50:14 +0800

    refactor: flatten legacy compat indexes

--- src/pocketbase/index.ts
+++ src/pocketbase/index.ts
@@ -1,18 +1,1 @@
-/**
- * PocketBase 服务统一导出
- */
-
-// 导出各个服务
-export { authService } from './auth'
-
-// 兼容旧的导出名称（方便迁移）
-export { authService as authApi } from './auth'
-// 导出客户端实例和工具函数
-export { mapErrorMessage, pb } from './client'
-export { filesService } from './files'
-export { filesService as filesApi } from './files'
-
-export { notesService } from './notes'
-export { notesService as notesApi } from './notes'
-export { usersService } from './users'
-export { usersService as userApi } from './users'
+export * from '@/shared/api/pocketbase'
