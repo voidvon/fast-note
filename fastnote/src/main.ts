3e8ede5c9d39ff47942be78fbb59147ba550e5ad
voidvon <x@0122.vip>
2026-04-13 22:51:07 +0800

    chore(debug): enable vconsole in production

--- fastnote/src/main.ts
+++ fastnote/src/main.ts
@@ -1,7 +1,16 @@
+import VConsole from 'vconsole'
 import { bootstrapApp } from '@/app/bootstrap/bootstrap-app'
 import { createVueApp } from '@/app/providers/create-vue-app'
 import '@/app/styles'
 import 'core-js/stable/array/to-sorted'
+
+const globalWindow = window as typeof window & {
+  __fastnoteVConsole?: VConsole
+}
+
+if (!globalWindow.__fastnoteVConsole) {
+  globalWindow.__fastnoteVConsole = new VConsole()
+}
 
 const { app, router } = createVueApp()
 
